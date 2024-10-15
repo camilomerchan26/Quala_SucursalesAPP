dcb3eb4d7f1e1decbf9de63cbb2dcf267759d746
camilomerchan26 <76219121+camilomerchan26@users.noreply.github.com>
2024-10-15 10:08:14 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
+  production: true,
+  webApiBaseUrl: 'https://localhost:7008/',
+  myApiUrlConsulta:'api/Consulta/',
+  opcion:'15',
+  myAuthUrl: 'api/auth/Login/'
 
 
   };
